b1a656a926064c0fe278ff39016b53fe3c6868b0
MatteD04 <matteo.dantonio04@gmail.com>
2024-03-18 12:38:57 +0100

    feat: generate random number

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@
 //Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
 //ed emetto un messaggio in console con il numero della cella cliccata.
 
+
+//In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+//La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+//generare 16 numeri casuali
+let bombList = [];
+for(let i = 0; i < 16; i++){
+
+   let bombNumber = Math.floor(Math.random() * 100) + 1;
+   //se il numero non è presente nell'array lo inserisco
+   if (bombList.includes(bombNumber) === false) {
+    bombList.push (bombNumber);
+   }
+}
+console.log(bombList);
+
+
 const mainGrid = document.querySelector('#grid');
 console.log(mainGrid);
 
@@ -17,7 +35,8 @@ btnPlay.addEventListener('click', function() {
         mainGrid.append(newSquare);
     }
 })
- 
+
+
 // Funzione che genera un quadrato
 // number -> rappresenta il numero
 // return: elemento del dom che rappresenta un quadrato
